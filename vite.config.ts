383a52bb10a6f59abe1278bc50f9bce696dcbfe1
Sachin Suresh <sachinsuresh00@gmail.com>
2025-04-13 02:13:43 +0530

    Build settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/teleparty-extension/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
